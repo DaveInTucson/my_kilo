d025a95d4e22325d40d677547ac9a24eb33ae627
dave.in.tucson.az@gmail.com <dave.in.tucson.az@gmail.com>
2017-04-17 01:02:03 -0700

    initial implementation

--- src/kilo.c
+++ src/kilo.c
@@ -1,4 +1,24 @@
+#include "terminal.h"
+#include "output.h"
+#include "editor_state.h"
+#include "die.h"
+
+void initEditor()
+{
+    if (getWindowSize(&g_editor_state.screenrows, &g_editor_state.screencols) == -1)
+        die("getWindowSize");
+}
+
 int main()
 {
+    enableRawMode();
+    initEditor();
+
+    while (1)
+    {
+        editorRefreshScreen();
+        editorProcessKeypress(editorReadKey());
+    }
+    
     return 0;
 }
